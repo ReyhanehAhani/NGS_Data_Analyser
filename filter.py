8716a2876a6c91a84ba018c71e91bf0281829dba
Reyhaneh <Reyhaneh.ahani@gmail.com>
2022-07-07 15:42:01 +0430

    Add file for filtering columns and rows based on requiremnts

---
+++ filter.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+def filter_by_parent(df, parent): 
+    # Keep Hom Iranome if it's NaN or 0
+    df = df[(df['Hom Iranome'] == '0') | (df['Hom Iranome'] == '.')]
+
+    # Keep refGene if isn't intronic
+    df = df[df['Func.refGene'] != 'intronic']
+
+    # Convert '.' to NaN, select columns that are NaN or less than 80, convert NaN back to '.'
+    df['Het Iranome'] = pd.to_numeric(df['Het Iranome'], errors='coerce')
+    df = df[(df['Het Iranome'].isna()) | (df['Het Iranome'] < 80)]
+    df['Het Iranome'] = df['Het Iranome'].fillna('.')
+
+    # Convert '.' to NaN, select columns that are NaN or less than 40, convert NaN back to '.'
+    df['Het Our DB'] = pd.to_numeric(df['Het Our DB'], errors='coerce')
+    df = df[(df['Het Our DB'].isna()) | (df['Het Our DB'] < 40)]
+    df['Het Our DB'] = df['Het Our DB'].fillna('.')
+
+    # Insert parent column
+    zygosity_index = df.columns.get_loc('Zygosity')
+    df.insert(zygosity_index, 'Parent', parent)
+
+
+df = pd.read_csv('father_mother/filtered_MOT_T_4904_2.csv')
+filter_by_parent(df, 'mother')
+df.to_csv('father_mother/NAME.csv', index=False)
+
+df = pd.read_csv('father_mother/filtered_MOT_T_4905_2.csv')
+filter_by_parent(df, 'father')
+df.to_csv('father_mother/NAME.csv', index=False)
