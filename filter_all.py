c631125239e046f5b18938eae9f7bc59f58d87a0
Reyhaneh Ahani <88882191+ReyhanehAhani@users.noreply.github.com>
2022-07-17 21:25:25 +0430

    Update filter_all.py

--- filter_all.py
+++ filter_all.py
@@ -1,3 +1,4 @@
+from tkinter import E
 import pandas as pd
 import numpy as np 
 
@@ -5,7 +6,7 @@ import argparse
 import io
 import pathlib
 
-from typing import Callable, Tuple, List, Union
+from typing import Callable, Tuple, List, Union, Dict
 
 Path = Union[str, pathlib.Path]
 
@@ -26,8 +27,29 @@ class GeneralParser():
     def success(self, message: str):
         print(message)
 
+    def read_OMIMfile(self, path: Path) -> Dict[str, str]: 
+            raw_genes: Dict[str, List[str]] = {}
+            filtered_genes: Dict[str, str] = {}
+
+            with open(path) as f: 
+                for line in f:
+                    line = line.strip().split('\t')
+
+                    if raw_genes.get(line[3]): 
+                        raw_genes[line[3]].append(line[2].strip())
+                    else: 
+                        raw_genes[line[3]] = [line[2]]
+
+            for key, item in raw_genes.items():
+                if 'AR' in item: 
+                    filtered_genes[key] = 'AR'
+                elif 'AD' in item:
+                    filtered_genes[key] = 'AD'
+
+            return filtered_genes
+
     def save_xlsx(self, dataframes: List[Tuple[pd.DataFrame, str]], output: Path):
-        current_row = 0
+        current_row: int = 0
         writer = pd.ExcelWriter(output, engine='xlsxwriter')
 
         # Get the worksheet and create the style
@@ -56,7 +78,6 @@ class GeneralParser():
     def read_faulty_csv(self, path: Path) -> pd.DataFrame: 
         df: pd.DataFrame | None = None
         columns: List[str] | None = None
-        max_columns: int = 0
 
         with open(path, 'r') as f:
             columns = list(dict.fromkeys([x for x in f.readline().strip().split(',') if x != '']))
@@ -86,7 +107,7 @@ class GeneralParser():
         
         return df
 
-    def read_csv(self, path: Path, parent: str, data_filter: Callable[[pd.DataFrame, str], pd.DataFrame]) -> pd.DataFrame:
+    def read_csv(self, path: Path, parent: str, data_filter: Callable[[pd.DataFrame, str, bool], pd.DataFrame], keep_intronic: bool = False) -> pd.DataFrame:
         try:
             df = pd.read_csv(path, index_col=False)
         except:
@@ -95,7 +116,7 @@ class GeneralParser():
         
         self.success(f'{path} was read successfully')
 
-        return data_filter(df, parent)
+        return data_filter(df, parent, keep_intronic)
 
     def concat_dataframes(self, dataframes: List[Tuple[pd.DataFrame, str]]):
         dataframes = [df.astype(str).reset_index(drop=True) for df in dataframes]
@@ -121,9 +142,13 @@ class GeneralParser():
         
         return not np.array_equal(father[match_columns].values, mother[match_columns].values)
 
-    def mother_and_child_share_gene(self, row: pd.Series):
+    def mother_and_child_share_gene(self, row: pd.Series, check_zygosity: bool = True):
         row = row.sort_values('Parent')
-        return (tuple(row['Parent'].values) == ('child', 'mother')) and (tuple(row['Zygosity'].values) == ('hom', 'het'))
+
+        if check_zygosity:
+            return (tuple(row['Parent'].values) == ('child', 'mother')) and (tuple(row['Zygosity'].values) == ('hom', 'het'))
+        else: 
+            return (tuple(row['Parent'].values) == ('child', 'mother'))
 
     def mother_father_and_child_do_not_share_gene(self, row: pd.Series):
         row = row.sort_values('Parent')
@@ -147,10 +172,11 @@ class GeneralParser():
                 return True
         return False
 
-    def filter_normal(self, df: pd.DataFrame, parent: str) -> pd.DataFrame:
+    def filter_normal(self, df: pd.DataFrame, parent: str, keep_intronic: bool = False) -> pd.DataFrame:
         df = df[(df['Hom Iranome'] == '0') | (df['Hom Iranome'] == '.')]
         
-        df = df[df['Func.refGene'] != 'intronic']
+        if not keep_intronic:
+            df = df[df['Func.refGene'] != 'intronic']
 
         df['Het Iranome'] = pd.to_numeric(df['Het Iranome'], errors='coerce')
         df = df[(df['Het Iranome'].isna()) | (df['Het Iranome'] < 80)]
@@ -165,7 +191,7 @@ class GeneralParser():
 
         return df
 
-    def filter_path(self, df: pd.DataFrame, parent: str) -> pd.DataFrame:
+    def filter_path(self, df: pd.DataFrame, parent: str, keep_intronic: bool = True) -> pd.DataFrame:
 
         df = df[(df['Hom Iranome'] == '0') | (df['Hom Iranome'] == '.')]
         df = df[~(df['CLNSIG'].str.contains('Benign', case=False))]
@@ -186,16 +212,18 @@ class GeneralParser():
 class FatherMotherParser(GeneralParser):
     def __init__(self, mother: Path, father: Path, 
                        mother_path: Path, father_path: Path, 
-                       output: Path):
+                       output: Path,
+                       keep_intronic: bool = False):
         self.mother = mother
         self.father = father
         self.mother_path = mother_path
         self.father_path = father_path
         self.output = output
+        self.keep_intronic = keep_intronic
     
     def run(self): 
-        mother = self.read_csv(self.mother, 'mother', self.filter_normal)
-        father = self.read_csv(self.father, 'father', self.filter_normal)
+        mother = self.read_csv(self.mother, 'mother', self.filter_normal, self.keep_intronic)
+        father = self.read_csv(self.father, 'father', self.filter_normal, self.keep_intronic)
 
         mother_path = self.read_csv(self.mother_path, 'mother', self.filter_path)
         father_path = self.read_csv(self.father_path, 'father', self.filter_path)
@@ -232,16 +260,18 @@ class FatherMotherParser(GeneralParser):
 class MotherChildParser(GeneralParser):
     def __init__(self, mother: Path, child: Path, 
                        mother_path: Path, child_path: Path, 
-                       output: Path):
+                       output: Path,
+                       keep_intronic: bool = False):
         self.mother = mother
         self.child = child
         self.mother_path = mother_path
         self.child_path = child_path
         self.output = output
+        self.keep_intronic = keep_intronic
     
     def run(self): 
-        mother = self.read_csv(self.mother, 'mother', self.filter_normal)
-        child = self.read_csv(self.child, 'child', self.filter_normal)
+        mother = self.read_csv(self.mother, 'mother', self.filter_normal, self.keep_intronic)
+        child = self.read_csv(self.child, 'child', self.filter_normal, self.keep_intronic)
 
         mother_path = self.read_csv(self.mother_path, 'mother', self.filter_path)
         child_path = self.read_csv(self.child_path, 'child', self.filter_path)
@@ -250,22 +280,15 @@ class MotherChildParser(GeneralParser):
         path_df = self.concat_dataframes([mother_path, child_path])
 
         shared_mother_child_gene = normal_df.groupby(self.match_columns).filter(self.mother_and_child_share_gene)
-        shared_mother_child_path = path_df.groupby([x for x in self.match_columns if x != 'Zygosity']).filter(self.mother_and_child_share_gene)
         not_shared_mother_child_gene = normal_df.groupby('Gene.refGene').filter(self.mother_father_and_child_do_not_share_gene)
         not_shared_mother_child_path = path_df.groupby('Gene.refGene').filter(self.mother_father_and_child_do_not_share_gene)
-        
-        shared_mother_child = self.concat_dataframes([
-            shared_mother_child_gene, 
-            shared_mother_child_path
-        ])
-
-        
 
         dataframes = [
-            (shared_mother_child, 'موارد مشترک در مادر و فرزند'),
+            (shared_mother_child_gene, 'موارد مشترک در مادر و فرزند'),
             (not_shared_mother_child_gene, 'موارد غیرمشترک در فرزند (‌فقط فرزند)'),
             (not_shared_mother_child_path, 'موارد پاتوژن فرزند'),
-            (path_df[(path_df['Parent'] == 'mother') & (path_df['Zygosity'] == 'hom')], 'موارد پاتوژن مادر')
+            (path_df[(path_df['Parent'] == 'mother') & (path_df['Zygosity'] == 'hom')], 'موارد پاتوژن مادر'),
+            (path_df.groupby(self.match_columns).filter(lambda x: self.mother_and_child_share_gene(x, False)), 'احتمال ناقل بودن در مادر و فرزند')
         ]
 
         self.save_xlsx(dataframes, self.output)
@@ -276,7 +299,8 @@ class FatherMotherChildParser(GeneralParser):
     def __init__(self, 
                  mother: Path, father: Path, child: Path,
                  mother_path: Path, father_path: Path, child_path: Path,
-                 output: Path): 
+                 output: Path,
+                 keep_intronic: bool = False): 
         self.mother = mother
         self.father = father
         self.child = child
@@ -284,11 +308,12 @@ class FatherMotherChildParser(GeneralParser):
         self.father_path = father_path
         self.child_path = child_path
         self.output = output
+        self.keep_intronic = keep_intronic
 
     def run(self): 
-        father = self.read_csv(self.father, 'father', self.filter_normal)
-        mother = self.read_csv(self.mother, 'mother', self.filter_normal)
-        child = self.read_csv(self.child, 'child', self.filter_normal)
+        father = self.read_csv(self.father, 'father', self.filter_normal, self.keep_intronic)
+        mother = self.read_csv(self.mother, 'mother', self.filter_normal, self.keep_intronic)
+        child = self.read_csv(self.child, 'child', self.filter_normal, self.keep_intronic)
 
         father_path = self.read_csv(self.father_path, 'father', self.filter_path)
         mother_path = self.read_csv(self.mother_path, 'mother', self.filter_path)
@@ -339,6 +364,7 @@ class FatherMotherChildParser(GeneralParser):
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--keep-intronic', action=argparse.BooleanOptionalAction, help="Skip filtering introinic values", default=False)
     subparsers = parser.add_subparsers(dest='mode', required=True)
 
     father_mother = subparsers.add_parser('father_mother', help="Filter the father_mother dataset")
@@ -352,6 +378,7 @@ def main():
     mother_child.add_argument('child', help="The file address for the child gene csv file")
     mother_child.add_argument('mother_path', help="The file address for the mother pathogen csv file")
     mother_child.add_argument('child_path', help="The file address for the child pathogen csv file")
+    #mother_child.add_argument('omim', help="The file address for the omim txt file")
     
     father_mother_child = subparsers.add_parser('father_mother_child', help="Filter the father_mother_child dataset")
     father_mother_child.add_argument('father', help="The file address for the father gene csv file")
@@ -360,18 +387,19 @@ def main():
     father_mother_child.add_argument('father_path', help="The file address for the father pathogen csv file")
     father_mother_child.add_argument('mother_path', help="The file address for the mother pathogen csv file")
     father_mother_child.add_argument('child_path', help="The file address for the child pathogen csv file")
-    
+    #father_mother_child.add_argument('omim', help="The file address for the omim txt file")
+
     args = parser.parse_args()
     
     if args.mode == 'father_mother':
         file_name = generate_file_name(args.father, args.mother)
-        FatherMotherParser(args.mother, args.father, args.mother_path, args.father_path, file_name).run()
+        FatherMotherParser(args.mother, args.father, args.mother_path, args.father_path, file_name, args.keep_intronic).run()
     elif args.mode == 'mother_child':
         file_name = generate_file_name(args.child, args.mother)
-        MotherChildParser(args.mother, args.child, args.mother_path, args.child_path, file_name).run()
+        MotherChildParser(args.mother, args.child, args.mother_path, args.child_path, file_name, args.keep_intronic).run()
     elif args.mode == 'father_mother_child': 
         file_name = generate_file_name(args.father, args.child, args.mother)
-        FatherMotherChildParser(args.mother, args.father, args.child, args.mother_path, args.father_path, args.child_path, file_name).run()
+        FatherMotherChildParser(args.mother, args.father, args.child, args.mother_path, args.father_path, args.child_path, file_name, args.keep_intronic).run()
 
 if __name__ == "__main__":
     main()
